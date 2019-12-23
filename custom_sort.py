c6b2ee27d693052515e594469f47016a5f60ac1f
kharedev <31240472+kharedev@users.noreply.github.com>
2019-12-23 21:26:23 +0530

    Update custom_sort.py

--- custom_sort.py
+++ custom_sort.py
@@ -1,3 +1,29 @@
+'''
+Custom Sort
+Description
+You have been given a lot of words, all different. You are to sort them as per the following requirements:
+Sort the words by length: the longest word first.
+If the length of two words is the same, compare the first letter of both the words. The letter that is lower on the lexicographical order is placed above the one that appears higher on the order.
+If a tie still exists, then compare the last letters. The letter that is lower on the lexicographical order is placed above the one that appears higher on the order.
+If a tie still exists, compare the second letters. The letter that is lower on the lexicographical order is placed above the one that appears higher on the order
+If a tie still exists, compare the second last letters. The letter that is lower on the lexicographical order is placed above the one that appears higher on the order
+And so on.
+Note- Lexicographical order is (A(lowest), B, C, D, … X, Y, Z(highest) )
+
+
+Input: A list of words.
+
+Output: A list of the same words sorted according to the instructions in the question.
+
+
+Sample input
+['laptop',   'Mobile',   'moaile',   'mobize', 'Car']
+Sample output
+['laptop', 'moaile', 'Mobile', 'mobize', 'Car']
+
+Please refer to the sample input and output in case of any confusion
+'''
+
 import ast
 import sys
 
